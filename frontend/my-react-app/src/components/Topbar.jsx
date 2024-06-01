039d66d880441c4af887e3e0ea3d918110939acb
ChristinaRaichel <celestianc.73@gmail.com>
2024-06-01 05:44:29 +0100

    frontend update

--- frontend/my-react-app/src/components/Topbar.jsx
+++ frontend/my-react-app/src/components/Topbar.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 
 function Topbar() {
   return (
-    <div className='font-bold font-4xl text-blue-950' >
-        CODEGEN 
+    <div className='flex justify-between items-center px-4 py-2'>
+    <div className='flex items-center' >
+      <h1 className='font-bold font-xl text-blue-950' >
+        CODEGEN </h1>
+    </div>
     </div>
   )
 }
