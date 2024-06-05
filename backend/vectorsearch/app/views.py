020fde45ca71ec7f7a7d0a5365b047c0f4a4ec69
ChristinaRaichel <celestianc.73@gmail.com>
2024-06-05 10:05:46 +0100

    updates in views.py, hero and search components edited

--- backend/vectorsearch/app/views.py
+++ backend/vectorsearch/app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ReactView(APIView):
+    def get(self,request):
+        output = 'hi all'
+        return Response(output)
+    
+    def post(self,request):
+        result = {'update added to json request'}
+        response_data = {
+                        'output': 'success',
+                        'explanation': 'File received and processed successfully',
+                        'pesudo_code': result
+                    }
+        return Response(response_data)
+    
